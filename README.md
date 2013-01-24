5ddfc7672f57448d18c5d33199df813e5c7eca81
Michal Belica <devel@beli.sk>
2013-01-24 01:08:08 +0100

    socks4 finished, foreground only

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 smallSocks
 ==========
 
-Small SOCKS server implementation in Python
+Small SOCKS server implementation in Python.
 
+Project status
+--------------
+
+These are the current features and limitations:
+
+  * SOCKS v4 protocol is supported
+  * forward operation only (no daemon)
+  * listens on `localhost`, port *1080*
--- smallsocks.py
+++ smallsocks.py
@@ -2,25 +2,47 @@
 
 import SocketServer
 import struct
+import socket
+import select
+import signal
+from pprint import pprint
 
 class ThreadTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
-    pass
+    allow_reuse_address = True
+    timeout = 1
 
 class Disconnect(Exception):
     pass
+
+def recv_strz(sock, maxlen = 65530):
+    """Receive a zero terminated string on socket"""
+    data = ''
+    l = 0
+    while l < maxlen:
+        c = sock.recv(1)
+        ld = len(c)
+        if ld == 0:
+            raise Disconnect()
+        elif ld != 1:
+            raise ValueError()
+        elif ord(c) == 0:
+            break
+        data += d
+        l += ld
+    return data
 
 def recv_all(sock, length):
     """Receive a fixed number of bytes from socket, iteratively if needed"""
     data = ''
     l = 0
     while l < length:
-        # gather required data from socket
         d = sock.recv(length - l)
         ld = len(d)
         if ld == 0:
             raise Disconnect
         data += d
         l += ld
+    return data
 
 def bin2int(data):
     """Convert binary unsigned int to python integer
@@ -34,34 +56,86 @@ def bin2int(data):
             4: 'L',
             8: 'Q',
             }
-    (i,) = struct.unpack('!'.data_types[len(data)], data)
+    (i,) = struct.unpack('!' + data_types[len(data)], data)
     return i
 
 def recv_socks_request(sock):
-    data = recv_all(sock, 9)
+    """Receive SOCKS4 request on socket and return it's details"""
+    data = recv_all(sock, 8)
     req = {
             'ver': bin2int(data[0:1]),
             'cmd': bin2int(data[1:2]),
             'port': bin2int(data[2:4]),
             'IP': socket.inet_ntoa(data[4:8]),
+            'user': recv_strz(sock, 254)
             }
     return req
 
+def send_socks_response(sock, status = True):
+    data = struct.pack('!BBHL', 0, 0x5a if status else 0x5b, 0, 0)
+    sock.sendall(data)
+
 class SocksTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
+        sock = self.request
         print "{} connected".format(self.client_address[0])
-        req = recv_socks_request(self.request)
+        req = recv_socks_request(sock)
         pprint(req)
+        if req['ver'] != 4:
+            print "%s unsupported protocol version detected %d" % \
+                    (self.client_address[0], req['ver'])
+            return
+        elif req['cmd'] != 1:
+            print "%s unsupported command %d" % \
+                    (self.client_address[0], req['cmd'])
+            send_socks_response(sock, False)
+            return
+        try:
+            outsock = socket.create_connection((req['IP'], req['port']))
+        except Exception as e:
+            send_socks_response(sock, False)
+            print "%s connect failed:" % (self.client_address[0])
+            pprint(e)
+            return
+        send_socks_response(sock)
         while True:
-            data = self.request.recv(1024)
-            if len(data) == 0:
-                print "{} disconnected".format(self.client_address[0])
-                break
-            print "{} wrote:".format(self.client_address[0])
-            print data.strip()
-            self.request.sendall(data.upper())
+            (rtr, rtw, err) = select.select([sock, outsock], [], [sock, outsock], 1)
+            if shutdown: break
+            for s in rtr:
+                try:
+                    data = s.recv(1024)
+                except Exception as e:
+                    print "%s receive failed:" % (self.client_address[0])
+                    pprint(e)
+                    return
+                if len(data) == 0:
+                    print "{} disconnected".format(self.client_address[0])
+                    return
+                if s == sock:
+                    outsock.sendall(data)
+                elif s == outsock:
+                    sock.sendall(data)
+                else:
+                    print "Unknown socket found!"
+                    return
+
+shutdown = False
+
+def sighandler(signum, frame):
+    global shutdown
+    if signum == signal.SIGINT or signum == signal.SIGTERM:
+        shutdown = True
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 1080
     server = ThreadTCPServer((HOST, PORT), SocksTCPHandler)
-    server.serve_forever()
+    signal.signal(signal.SIGINT, sighandler)
+    #signal.signal(signal.SIGTERM, sighandler)
+    while not shutdown:
+        try:
+            server.handle_request()
+        except select.error as e:
+            if e[0] == 4:
+                print "handle_request interrupted"
+            else:
+                raise
